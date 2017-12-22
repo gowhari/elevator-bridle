24b48e88293c903456f44acbb2c84fd9c453ed58
iman gowhari <gowhari@gmail.com>
2017-12-22 16:49:15 +0330

    version 1.0 deployed

--- elevator/elevator.ino
+++ elevator/elevator.ino
@@ -2,19 +2,28 @@
 see MFRC522 library examples for pin layout
 */
 
-#include <SPI.h>
 #include <EEPROM.h>
-#include <MFRC522.h>
+#include <MFRC522Helper.h>
 
 #define RST_PIN 9
 #define SS_PIN 10
+#define AUTHORIZE_PIN 7
+#define BEEP_PIN 8
 #define CARD_TYPE_INVALID 0
 #define CARD_TYPE_UNIT 1
-#define CARD_TYPE_WRITE 2
-#define CARDS_ARRAY_BYTES 4
+#define CARD_TYPE_COMMAND 2
+#define CMD_UPDATE 1
+#define CMD_ERASE_AND_UPDATE 2
+#define CMD_SET_NO_CONTROL_MODE 3
+#define CMD_CLEAR_NO_CONTROL_MODE 4
+#define CMD_BEEP_FREQ_INC 5
+#define CMD_BEEP_FREQ_DEC 6
+#define CARDS_ARRAY_BYTES 512
 #define CARDS_ARRAY_NO_OF_CARDS (CARDS_ARRAY_BYTES * 8)
-#define CARDS_ARRAY_EEPROM_ADDR 0
-
+#define EEPROM_ADDR_CARDS_ARRAY 0
+#define BEEP_FREQ_DEFAULT 2000
+#define BEEP_FREQ_MIN 1000
+#define BEEP_FREQ_MAX 2500
 
 struct Card {
     byte type;
@@ -22,22 +31,39 @@ struct Card {
 };
 
 
+const int EEPROM_ADDR_OTHER_DATA = EEPROM_ADDR_CARDS_ARRAY + CARDS_ARRAY_BYTES;
+const int EEPROM_ADDR_NO_CONTROL_MODE = EEPROM_ADDR_OTHER_DATA + 0;
+const int EEPROM_ADDR_BEEP_FREQ = EEPROM_ADDR_OTHER_DATA + 1;
 byte cards_array[CARDS_ARRAY_BYTES];
 byte last_auth_block = 0;
-MFRC522 mfrc(SS_PIN, RST_PIN);
-MFRC522::MIFARE_Key mfrc_key;
+MFRC522Helper card_helper;
+byte auth_key_val[] = {90, 206, 26, 227, 18, 6};
+byte magic_num[] = {101, 214, 190, 66};
+bool no_control_mode = false;
+int beep_freq = BEEP_FREQ_DEFAULT;
 
 
 void setup() {
+    pinMode(AUTHORIZE_PIN, OUTPUT);
+    digitalWrite(AUTHORIZE_PIN, HIGH);
     Serial.begin(115200);
     while (!Serial);
+    load_config();
     SPI.begin();
-    mfrc.PCD_Init();
-    for (byte i = 0; i < 6; i++) {
-        mfrc_key.keyByte[i] = 0xFF;
-    }
+    card_helper.mfrc.PCD_Init();
+    memcpy(card_helper.mfrc_key.keyByte, auth_key_val, 6);
     load_cards_array_from_eeprom();
+    // dump_cards_array();
     Serial.println(F("Ready"));
+}
+
+
+void load_config() {
+    no_control_mode = (bool)EEPROM.read(EEPROM_ADDR_NO_CONTROL_MODE);
+    beep_freq = (byte)EEPROM.read(EEPROM_ADDR_BEEP_FREQ) * 10;
+    if (beep_freq < BEEP_FREQ_MIN || beep_freq > BEEP_FREQ_MAX) {
+        beep_freq = BEEP_FREQ_DEFAULT;
+    }
 }
 
 
@@ -52,7 +78,7 @@ void dump_byte_array(byte *buffer, byte bufferSize) {
 void dump_cards_array() {
     char s[50];
     for (byte i = 0; i < CARDS_ARRAY_NO_OF_CARDS; i++) {
-        sprintf(s, "%d: %d", i, read_state_of_card_from_ram(i));
+        sprintf(s, (char*)F("%d: %d"), i, read_state_of_card_from_ram(i));
         Serial.println(s);
     }
 }
@@ -67,24 +93,57 @@ byte get_checksum(byte* data, byte size) {
 }
 
 
+void play_and_wait(word freq, word duration, word wait_after) {
+    tone(BEEP_PIN, freq, duration);
+    delay(duration);
+    noTone(BEEP_PIN);
+    if (wait_after > 0) {
+        delay(wait_after);
+    }
+}
+
+
 void play_success_beep() {
-    Serial.println("play_success_beep");
+    play_and_wait(beep_freq, 100, 0);
 }
 
 
 void play_failure_beep() {
-    Serial.println("play_failure_beep");
+    play_and_wait(beep_freq, 200, 200);
+    play_and_wait(beep_freq, 200, 200);
+    play_and_wait(beep_freq, 200, 0);
+}
+
+
+void change_beep_freq(int dir) {
+    int new_freq = beep_freq + 10 * dir;
+    Serial.print(beep_freq);
+    Serial.print(" ");
+    Serial.println(new_freq);
+    if (new_freq >= BEEP_FREQ_MIN && new_freq <= BEEP_FREQ_MAX) {
+        beep_freq = new_freq;
+        EEPROM.update(EEPROM_ADDR_BEEP_FREQ, beep_freq / 10);
+    }
+    play_success_beep();
+}
+
+
+void erase_eeprom() {
+    int n = EEPROM.length();
+    for (int i = 0; i < n; i++) {
+        EEPROM.write(i, 0);
+    }
 }
 
 
 /*
 return corresponding bit of cards array from ram for this cord no
 */
-boolean read_state_of_card_from_ram(int card_no) {
+bool read_state_of_card_from_ram(int card_no) {
     int byte_index = card_no / 8;
     byte bit_index = card_no % 8;
     byte mask = 1 << bit_index;
-    return (boolean)(cards_array[byte_index] & mask);
+    return (bool)(cards_array[byte_index] & mask);
 }
 
 
@@ -93,14 +152,24 @@ void write_byte_to_ram_and_eeprom(int index, byte new_val) {
     if(new_val == old_val)
         return;
     cards_array[index] = new_val;
-    EEPROM.update(CARDS_ARRAY_EEPROM_ADDR + index, new_val);
+    EEPROM.update(EEPROM_ADDR_CARDS_ARRAY + index, new_val);
 }
 
 
 void load_cards_array_from_eeprom() {
     for(int i = 0; i < CARDS_ARRAY_BYTES; i++) {
-        cards_array[i] = EEPROM.read(CARDS_ARRAY_EEPROM_ADDR + i);
+        cards_array[i] = EEPROM.read(EEPROM_ADDR_CARDS_ARRAY + i);
     }
+}
+
+
+void lock_elevator() {
+    digitalWrite(AUTHORIZE_PIN, HIGH);
+}
+
+
+void unlock_elevator() {
+    digitalWrite(AUTHORIZE_PIN, LOW);
 }
 
 
@@ -108,110 +177,49 @@ void load_cards_array_from_eeprom() {
 enable elevator key pad for 30 seconds
 */
 void authorize() {
-    Serial.println("authorized!");
+    Serial.println(F("authorized!"));
+    unlock_elevator();
+    delay(10000);
+    lock_elevator();
 }
 
 
-boolean find_a_new_card() {
-    if (!mfrc.PICC_IsNewCardPresent()) {
-        return false;
+void decrypt_data(byte* data, byte data_size, byte* res) {
+    byte first_byte = data[0];
+    byte h_byte, l_byte, h_nib, l_nib, val;
+    bool use_low_nibble = (first_byte % 2 == 0);
+    for (int j = 0, i = 1; i < data_size; j++, i += 2) {
+        h_byte = data[i] ^ first_byte;
+        l_byte = data[i + 1] ^ first_byte;
+        if (use_low_nibble) {
+            l_nib = l_byte & 0x0f;
+            h_nib = h_byte & 0x0f;
+        }
+        else {
+            l_nib = l_byte >> 4;
+            h_nib = h_byte >> 4;
+        }
+        val = (h_nib << 4) | l_nib;
+        res[j] = val;
     }
-    if (!mfrc.PICC_ReadCardSerial()) {
-        return false;
-    }
-    MFRC522::PICC_Type picc_type = mfrc.PICC_GetType(mfrc.uid.sak);
-    if (picc_type != MFRC522::PICC_TYPE_MIFARE_MINI && picc_type != MFRC522::PICC_TYPE_MIFARE_1K &&
-        picc_type != MFRC522::PICC_TYPE_MIFARE_4K) {
-        Serial.print(F("invalid picc card: "));
-        Serial.println(mfrc.PICC_GetTypeName(picc_type));
-        return false;
-    }
-    return true;
 }
-
-
-byte get_trailer_block(byte block) {
-    return block / 4 * 4 + 3;
-}
-
-
-boolean authenticate_if_needed(byte block) {
-    if (last_auth_block == 0) {
-        return authenticate_to_block(block);
-    }
-    if (get_trailer_block(block) != last_auth_block) {
-        return authenticate_to_block(block);
-    }
-    return true;
-}
-
-
-boolean authenticate_to_block(byte block) {
-    byte trailer_block = get_trailer_block(block);
-    Serial.print("authenticate_to_block: ");
-    Serial.println(trailer_block);
-    MFRC522::StatusCode status;
-    status = mfrc.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, trailer_block, &mfrc_key, &(mfrc.uid));
-    if (status != MFRC522::STATUS_OK) {
-        Serial.print(F("authenticate failed: "));
-        Serial.println(mfrc.GetStatusCodeName(status));
-        return false;
-    }
-    last_auth_block = trailer_block;
-    return true;
-}
-
-
-boolean read_block_from_card(byte block, byte* buf) {
-    if (!authenticate_if_needed(block)) {
-        return false;
-    }
-    MFRC522::StatusCode status;
-    byte size = 18;
-    status = mfrc.MIFARE_Read(block, buf, &size);
-    if (status != MFRC522::STATUS_OK) {
-        Serial.print(F("read block failed: "));
-        Serial.println(mfrc.GetStatusCodeName(status));
-        return false;
-    }
-    return true;
-}
-
-
-boolean write_block_to_card(byte block, byte* buf) {
-    if (!authenticate_if_needed(block)) {
-        return false;
-    }
-    MFRC522::StatusCode status;
-    Serial.print(F("Writing data into block "));
-    Serial.print(block);
-    Serial.println(F(" ..."));
-    dump_byte_array(buf, 16);
-    Serial.println();
-    status = mfrc.MIFARE_Write(block, buf, 16);
-    if (status != MFRC522::STATUS_OK) {
-        Serial.print(F("MIFARE_Write() failed: "));
-        Serial.println(mfrc.GetStatusCodeName(status));
-    }
-    Serial.println();
-}
-
-
-void stop_card_communication() {
-    last_auth_block = 0;
-    mfrc.PICC_HaltA();
-    mfrc.PCD_StopCrypto1();
-}
-
 
 /*
-decrypt 16 bytes initial card data and check if it's correctly encrypted or no
-the return value is a Card struc, for invalid encryption return {CARD_TYPE_INVALID, 0}
+read 16 bytes initial card data and check if it's correctly encrypted or no
+the return value is a Card struct, for invalid encryption return {CARD_TYPE_INVALID, 0}
 */
-Card decrypt_first_block_data(byte* data) {
-    byte type = data[0];
-    int no = (((word)data[1]) << 8) | ((word)data[2]);
-    if(type != CARD_TYPE_UNIT && type != CARD_TYPE_WRITE) {
+Card read_first_block_data(byte* encrypted_data) {
+    byte data[16];
+    byte type;
+    int no;
+    decrypt_data(encrypted_data, 15, data);
+    dump_byte_array(data, 16);
+    Serial.println();
+    if (is_valid_card(data)) {
+        type = data[0];
+        no = (((word)data[1]) << 8) | ((word)data[2]);
+    }
+    else {
         type = CARD_TYPE_INVALID;
         no = 0;
     }
@@ -220,13 +228,43 @@ Card decrypt_first_block_data(byte* data) {
 }
 
 
+bool is_valid_card(byte* data) {
+    byte type = data[0];
+    byte* magic = &(data[3]);
+    if (magic[0] != magic_num[0] || magic[1] != magic_num[1] || magic[2] != magic_num[2] || magic[3] != magic_num[3])
+        return false;
+    if (type != CARD_TYPE_UNIT && type != CARD_TYPE_COMMAND)
+        return false;
+    return true;
+}
+
+
+void set_no_control_mode(bool val) {
+    no_control_mode = val;
+    EEPROM.update(EEPROM_ADDR_NO_CONTROL_MODE, (byte)val);
+    if (no_control_mode) {
+        unlock_elevator();
+    }
+    else {
+        lock_elevator();
+    }
+    play_success_beep();
+}
+
+
 void process_unit_card(Card card) {
-    boolean state = read_state_of_card_from_ram(card.no);
+    if (no_control_mode) {
+        Serial.println(F("no control mode"));
+        return;
+    }
+    bool state = read_state_of_card_from_ram(card.no);
     if (state == true) {
+        Serial.println(F("play_success_beep"));
         play_success_beep();
         authorize();
     }
     else {
+        Serial.println(F("play_failure_beep"));
         play_failure_beep();
     }
 }
@@ -244,19 +282,19 @@ bit 15 of index (bit 7 of high byte):
 byte 15:
   is checksum of all previous bytes (0 to 14)
 */
-boolean process_write_card(Card card) {
+bool process_update_card() {
     byte block = 4;
     byte data[18];
     byte i, high, low, value, chk;
     int index;
-    boolean is_more_data = true;
+    bool is_more_data = true;
     char str[100];
     while (block < 63 && is_more_data) {
-        if (!read_block_from_card(block, data))
+        if (!card_helper.read_block_from_card(block, data))
             return false;
         chk = get_checksum(data, 15);
         if (data[15] != chk) {
-            Serial.println("invalid checksum");
+            Serial.println(F("invalid checksum"));
             return false;
         }
         for (i = 0; i <= 12 ; i += 3) {
@@ -269,7 +307,7 @@ boolean process_write_card(Card card) {
             value = data[i + 2];
             index = ((high & 0x7f) << 8) | low;
             write_byte_to_ram_and_eeprom(index, value);
-            sprintf(str, "i=%02d  high=%02x  low=%02x  index=%02x  value=%02x", i, high, low, index, value);
+            sprintf(str, (char*)F("i=%02d  high=%02x  low=%02x  index=%02x  value=%02x"), i, high, low, index, value);
             Serial.println(str);
         }
         if(!is_more_data)
@@ -283,74 +321,49 @@ boolean process_write_card(Card card) {
 
 
 void process_new_card(byte* data) {
-    Card card = decrypt_first_block_data(data);
+    Card card = read_first_block_data(data);
     char s[50];
     sprintf(s, "card info: type=%d no=%d", card.type, card.no);
     Serial.println(s);
     if (card.type == CARD_TYPE_INVALID) {
-        Serial.println("invalid card");
-        return;
+        Serial.println(F("invalid card"));
     }
     else if (card.type == CARD_TYPE_UNIT) {
         process_unit_card(card);
     }
-    else if (card.type == CARD_TYPE_WRITE) {
-        process_write_card(card);
+    else if (card.type == CARD_TYPE_COMMAND && card.no == CMD_UPDATE) {
+        process_update_card();
+    }
+    else if (card.type == CARD_TYPE_COMMAND && card.no == CMD_ERASE_AND_UPDATE) {
+        erase_eeprom();
+        process_update_card();
+    }
+    else if (card.type == CARD_TYPE_COMMAND && card.no == CMD_SET_NO_CONTROL_MODE) {
+        set_no_control_mode(true);
+    }
+    else if (card.type == CARD_TYPE_COMMAND && card.no == CMD_CLEAR_NO_CONTROL_MODE) {
+        set_no_control_mode(false);
+    }
+    else if (card.type == CARD_TYPE_COMMAND && card.no == CMD_BEEP_FREQ_DEC) {
+        change_beep_freq(-1);
+    }
+    else if (card.type == CARD_TYPE_COMMAND && card.no == CMD_BEEP_FREQ_INC) {
+        change_beep_freq(+1);
+    }
+    else {
+        Serial.println(F("invalid command"));
     }
 }
 
 
 void loop() {
     byte buf[18];
-    if (!find_a_new_card())
+    if (!card_helper.find_a_new_card())
         return;
-    if (!authenticate_to_block(1))
+    if (!card_helper.authenticate_to_block(1))
         return;
-    if (read_block_from_card(1, buf)) {
+    if (card_helper.read_block_from_card(1, buf)) {
         process_new_card(buf);
     }
-    stop_card_communication();
-
-
-    // write data on write-card
-    // byte buf[][16] = {
-    //     {128, 0, 3, 128, 1, 15, 128, 2, 7, 0, 0, 0, 0, 0, 0, 145},
-    //     {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 15},
-    // };
-    // byte rows = sizeof(buf) / 16;
-    // if (!find_a_new_card())
-    //     return;
-    // if (!authenticate_to_block(7))
-    //     return;
-    // for(int i = 0; i < rows; i++) {
-    //     write_block_to_card(i + 4, buf[i]);
-    // }
-    // stop_card_communication();
-
-
-    // // write 1st record on card
-    // // byte buf[] = {CARD_TYPE_UNIT, 0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-    // byte buf[] = {CARD_TYPE_WRITE, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-    // if (!find_a_new_card())
-    //     return;
-    // if (!authenticate_to_block(1))
-    //     return;
-    // write_block_to_card(1, buf);
-    // stop_card_communication();
-
-
-    // // dump cards array
-    // dump_cards_array();
-    // Serial.println();
-    // delay(1000);
-    // exit(0);
-
-
-
-    // erase eeprom
-    // int n = EEPROM.length();
-    // for(int i = 0; i < n; i++) {
-    //     EEPROM.write(i, 0);
-    // }
-    // Serial.println("Done");
+    card_helper.stop_card_communication();
 }
